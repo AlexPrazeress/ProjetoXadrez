afe3dc8ec1d1f926b03b886c0d22dac839fc6938
AlexPrazeress <alexuniversal@outlook.com>
2019-12-02 23:29:28 -0300

    Criando excecoes para posicoes inexistentes

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 import xadrez.PosicaoDoXadrez;
+import xadrez.XadrezExcption;
 
 public class Programa {
 	
@@ -13,18 +15,28 @@ public class Programa {
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
 		while (true) {
-			Ui.impressaoDeTabuleiro(partidaDeXadrez.getPecas());
-			System.out.println();
-			System.out.println("Source: ");
-			PosicaoDoXadrez source = Ui.lerPosicaoDoUsuario(sc);
+			try {
+				Ui.limparTela();
+				Ui.impressaoDeTabuleiro(partidaDeXadrez.getPecas());
+				System.out.println();
+				System.out.println("Source: ");
+				PosicaoDoXadrez source = Ui.lerPosicaoDoUsuario(sc);
+				
+				System.out.println();
+				System.out.println("Target: ");
+				PosicaoDoXadrez target = Ui.lerPosicaoDoUsuario(sc);
 			
-			System.out.println();
-			System.out.println("Target: ");
-			PosicaoDoXadrez target = Ui.lerPosicaoDoUsuario(sc);
-			
-			PecaDeXadrez capturaDaPeca = partidaDeXadrez.ExecutarXadrez(source, target);
+				PecaDeXadrez capturaDaPeca = partidaDeXadrez.ExecutarXadrez(source, target);
+			}
+			catch(XadrezExcption e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
 	}
 
 }
